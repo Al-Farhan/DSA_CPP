98ec28988a71ea81bf78141acb8349d41643fa56
DeltaProjects <programmingwithfarhan@gmail.com>
2023-08-28 21:25:55 +0530

    Added new files

--- 01_firstCode.cpp
+++ 01_firstCode.cpp
@@ -8,3 +8,6 @@ int main() {
     cout<<sizeof(a)<<endl;
     return 0;
 }
+
+// What is .o file
+// compile hone k baad 1 .o file banti hai uske result se se machine code banta hai
--- 01_fristCode.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-
-    cout<<"Hello Farhan"<<endl;
-    return 0;
-}
